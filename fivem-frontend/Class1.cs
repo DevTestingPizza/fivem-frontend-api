6317f3d3bfdcfbaab61135dfe262208b45dd7bcf
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-06-18 16:55:59 +0200

    Add project files.

---
+++ fivem-frontend/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fivem_frontend
+{
+    public class Class1
+    {
+    }
+}
